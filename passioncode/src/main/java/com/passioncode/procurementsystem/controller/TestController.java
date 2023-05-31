81586e1178b8d00e4bd6b70c0a828094abb7b31b
soheeKim7 <okoksohee@gmail.com>
2023-05-31 11:20:59 +0900

    처음 패션코드 셋팅!
    아직 레이아웃 처리는 안한거고, 즉 헤더 푸터 그거 안한거고, 수진이가 만들었던거 우리꺼에 적용만 시켜놓고, 돌려놓은 상태

---
+++ passioncode/src/main/java/com/passioncode/procurementsystem/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.passioncode.procurementsystem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+	
+	@GetMapping("layout/basic")
+	public void layoutTest() {
+		
+	}
+//	public String layoutTest() {
+//		return "layout/layoutTest";
+//	}
+
+}
