ae36520af884c81624743ee0133a127ec56e48e2
soheeKim7 <okoksohee@gmail.com>
2023-05-31 15:12:18 +0900

    례이아웃 정리!! (헤더,푸터 포함)

--- passioncode/src/main/java/com/passioncode/procurementsystem/PassioncodeApplication.java
+++ passioncode/src/main/java/com/passioncode/procurementsystem/PassioncodeApplication.java
@@ -1,6 +1,7 @@
 package com.passioncode.procurementsystem;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
